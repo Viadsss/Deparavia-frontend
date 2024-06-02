5cb6b9177e77db4dd1a7e53141f43e721305411f
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-06-02 21:14:14 +0800

    Connect returning Login API

--- src/components/admission/returning/LoginInfo.jsx
+++ src/components/admission/returning/LoginInfo.jsx
@@ -1,10 +1,17 @@
 import {
   Button,
   FormControl,
+  FormErrorIcon,
+  FormErrorMessage,
   FormLabel,
   Heading,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
+import { IconEye, IconEyeClosed } from "@tabler/icons-react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
@@ -15,6 +22,8 @@ export default function LoginInfo({
   handleNextStep,
 }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -22,6 +31,7 @@ export default function LoginInfo({
       ...prev,
       [name]: name === "patientID" ? value.toUpperCase() : value,
     }));
+    setError("");
   };
 
   const handleSumbit = async (e) => {
@@ -31,8 +41,14 @@ export default function LoginInfo({
     console.log(loginFormData);
     try {
       // TODO: POST (Check Pass and ID to the Database) and check if successful
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await axios.post(
+        "http://localhost:8080/api/admission/returning/login",
+        loginFormData
+      );
       handleNextStep();
+    } catch (err) {
+      const message = err.response.data;
+      setError(message);
     } finally {
       setIsLoading(false);
     }
@@ -44,7 +60,7 @@ export default function LoginInfo({
         I. Login
       </Heading>
       <form onSubmit={handleSumbit}>
-        <FormControl isRequired mt="12px">
+        <FormControl isRequired mt="12px" isInvalid={Boolean(error)}>
           <FormLabel>Patient ID</FormLabel>
           <Input
             type="text"
@@ -54,17 +70,28 @@ export default function LoginInfo({
             value={loginFormData.patientID}
             onChange={handleChange}
           />
-        </FormControl>
-        <FormControl isRequired mt="12px">
           <FormLabel>Password</FormLabel>
-          <Input
-            type="password"
-            name="password"
-            maxLength={20}
-            value={loginFormData.password}
-            onChange={handleChange}
-          />
+          <InputGroup>
+            <Input
+              type={show ? "text" : "password"}
+              name="password"
+              maxLength={20}
+              value={loginFormData.password}
+              onChange={handleChange}
+            />
+            <InputRightElement>
+              <IconButton
+                icon={show ? <IconEye /> : <IconEyeClosed />}
+                onClick={() => setShow(!show)}
+              />
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>
+            <FormErrorIcon />
+            {error}
+          </FormErrorMessage>
         </FormControl>
+
         <Button isLoading={isLoading} type="submit" mt="12px">
           Next
         </Button>
