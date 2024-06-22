db4286ae621c10e2e0db5ce4607f2b701416b733
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-06-22 19:04:07 +0800

    Add Multiple Doctor Views for doctor Record

--- src/components/admin/AdminDashBoard.jsx
+++ src/components/admin/AdminDashBoard.jsx
@@ -8,7 +8,6 @@ import {
   TabPanels,
   Tabs,
   useDisclosure,
-  useToast,
 } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import { useColorModeValue } from "@chakra-ui/react";
@@ -16,21 +15,21 @@ import { useEffect, useState } from "react";
 import AdmissionRecord from "./admissionRecord/AdmissionRecord";
 import DoctorRecord from "./doctorRecord/DoctorRecord";
 import PatientRecord from "./patientRecord/PatientRecord";
-import { filterDoctorData } from "../../utils/funcUtils";
 import VisitorRecord from "./visitorRecord/VisitorRecord";
 import { IconLogout } from "@tabler/icons-react";
-import axios from "axios";
 import MonthModal from "./MonthModal";
 import YearModal from "./YearModal";
+import axios from "axios";
 
 // Jhana
 
 export default function AdminDashBoard({ setIsLogin }) {
   const [doctorData, setDoctorData] = useState([]);
-  const [doctorFilteredData, setDoctorFilteredData] = useState([]);
-  const [doctorSearchTerm, setDoctorSearchTerm] = useState("");
-  const [isPendingDoctor, setIsPendingDoctor] = useState(true);
-  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
   const {
     isOpen: isOpenMonth,
     onOpen: onOpenMonth,
@@ -45,64 +44,21 @@ export default function AdminDashBoard({ setIsLogin }) {
   const bgCard = useColorModeValue("white", "gray.800");
   const borderCard = useColorModeValue("gray.200", "gray.600");
   const theme = useColorModeValue("chakraLight", "chakraDark");
-  const toast = useToast();
 
-  useEffect(() => {
-    getDoctorData();
-    getDoctorTotal();
-  }, []);
-
-  const handleLogOut = () => {
-    setIsLogin(false);
-  };
-
-  const getDoctorTotal = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:8080/api/admin/doctors/total"
-      );
-      const data = response.data;
-      const total = data.total;
-      setTotal(total);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const getDoctorData = async () => {
+  const getDoctorsData = async () => {
     try {
       const response = await axios.get(
         "http://localhost:8080/api/admin/doctors"
       );
       const data = response.data;
       setDoctorData(data);
-      setDoctorFilteredData(data);
     } catch (err) {
       console.error(err);
-    } finally {
-      setIsPendingDoctor(false);
     }
   };
 
-  const handleDoctorUpdate = async (toastDetails) => {
-    const fetchDoctorTable = async () => {
-      getDoctorData();
-      getDoctorTotal();
-    };
-
-    // Refetch admission data after successful PUT request
-    try {
-      toast.promise(fetchDoctorTable(), toastDetails);
-    } catch (err) {
-      console.error("Error fetching admissions:", err);
-    }
-  };
-
-  const handleDoctorSearch = (e) => {
-    setDoctorSearchTerm(e.target.value);
-    const search = e.target.value.toLowerCase();
-    const newRows = filterDoctorData(doctorData, search);
-    setDoctorFilteredData(newRows);
+  const handleLogOut = () => {
+    setIsLogin(false);
   };
 
   return (
@@ -139,15 +95,10 @@ export default function AdminDashBoard({ setIsLogin }) {
               <TabPanel>
                 <AdmissionRecord theme={theme} doctorData={doctorData} />
               </TabPanel>
-              <TabPanel>
+              <TabPanel>  
                 <DoctorRecord
                   theme={theme}
-                  doctorData={doctorFilteredData}
-                  isPending={isPendingDoctor}
-                  handleDoctorUpdate={handleDoctorUpdate}
-                  searchTerm={doctorSearchTerm}
-                  handleSearch={handleDoctorSearch}
-                  total={total}
+                  updateAdmissionDoctors={getDoctorsData}
                 />
               </TabPanel>
               <TabPanel>
--- src/components/admin/doctorRecord/DoctorRecord.jsx
+++ src/components/admin/doctorRecord/DoctorRecord.jsx
@@ -11,22 +11,286 @@ import {
   InputLeftElement,
   Text,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { IconSearch, IconUserPlus } from "@tabler/icons-react";
 import AddDrawer from "./AddDrawer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { filterDoctorData } from "../../../utils/funcUtils";
 
-export default function DoctorRecord({
-  theme,
-  doctorData,
-  isPending,
-  handleDoctorUpdate,
-  searchTerm,
-  handleSearch,
-  total,
-}) {
+export default function DoctorRecord({ theme, updateAdmissionDoctors }) {
+  const [isPending, setIsPending] = useState(true);
+  const [doctorData, setDoctorData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [total, setTotal] = useState(0);
-  const [admissionState, setAdmissionState] = useState(1);
+  const [doctorState, setDoctorState] = useState(1);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  useEffect(() => {
+    getDoctorsData();
+    getDoctorsTotal();
+  }, []);
+
+  const handleGetDoctors = async () => {
+    getDoctorsData();
+    getDoctorsTotal();
+    setDoctorState(1);
+  };
+
+  const handleGetOnDutyDoctors = async () => {
+    getDoctorsOnDutyData();
+    getDoctorsOnDutyTotal();
+    setDoctorState(2);
+  };
+
+  const handleGetOffDutyDoctors = async () => {
+    getDoctorsOffDutyData();
+    getDoctorsOffDutyTotal();
+    setDoctorState(3);
+  };
+
+  const handleGetActiveDoctors = async () => {
+    getDoctorsActiveData();
+    getDoctorsActiveTotal();
+    setDoctorState(4);
+  };
+
+  const handleGetInactiveDoctors = async () => {
+    getDoctorsInactiveData();
+    getDoctorsInactiveTotal();
+    setDoctorState(5);
+  };
+
+  const handleGetOnLeaveDoctors = async () => {
+    getDoctorsOnLeaveData();
+    getDoctorsOnLeaveTotal();
+    setDoctorState(6);
+  };
+
+  const getDoctorsData = async () => {
+    setIsPending(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/admin/doctors"
+      );
+      const data = response.data;
+      setDoctorData(data);
+      setFilteredData(data);
+      setSearchTerm("");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  const getDoctorsOnDutyData = async () => {
+    setIsPending(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/admin/doctors/onDuty"
+      );
+      const data = response.data;
+      setDoctorData(data);
+      setFilteredData(data);
+      setSearchTerm("");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  const getDoctorsOffDutyData = async () => {
+    setIsPending(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/admin/doctors/offDuty"
+      );
+      const data = response.data;
+      setDoctorData(data);
+      setFilteredData(data);
+      setSearchTerm("");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  const getDoctorsActiveData = async () => {
+    setIsPending(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/admin/doctors/active"
+      );
+      const data = response.data;
+      setDoctorData(data);
+      setFilteredData(data);
+      setSearchTerm("");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  const getDoctorsInactiveData = async () => {
+    setIsPending(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/admin/doctors/inactive"
+      );
+      const data = response.data;
+      setDoctorData(data);
+      setFilteredData(data);
+      setSearchTerm("");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  const getDoctorsOnLeaveData = async () => {
+    setIsPending(true);
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/admin/doctors/onLeave"
+      );
+      const data = response.data;
+      setDoctorData(data);
+      setFilteredData(data);
+      setSearchTerm("");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  const getDoctorsTotal = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/doctors/total`
+      );
+      const data = response.data;
+      const total = data.total;
+      setTotal(total);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getDoctorsOnDutyTotal = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/doctors/onDuty/total`
+      );
+      const data = response.data;
+      const total = data.total;
+      setTotal(total);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getDoctorsOffDutyTotal = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/doctors/offDuty/total`
+      );
+      const data = response.data;
+      const total = data.total;
+      setTotal(total);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getDoctorsActiveTotal = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/doctors/active/total`
+      );
+      const data = response.data;
+      const total = data.total;
+      setTotal(total);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getDoctorsInactiveTotal = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/doctors/inactive/total`
+      );
+      const data = response.data;
+      const total = data.total;
+      setTotal(total);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getDoctorsOnLeaveTotal = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/admin/doctors/onLeave/total`
+      );
+      const data = response.data;
+      const total = data.total;
+      setTotal(total);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleDataUpdate = async (toastDetails) => {
+    const fetchDoctorsData = async () => {
+      switch (doctorState) {
+        case 1:
+          handleGetDoctors();
+          break;
+        case 2:
+          handleGetOnDutyDoctors();
+          break;
+        case 3:
+          handleGetOffDutyDoctors();
+          break;
+        case 4:
+          handleGetActiveDoctors();
+          break;
+        case 5:
+          handleGetInactiveDoctors();
+          break;
+        case 6:
+          handleGetOnLeaveDoctors();
+          break;
+        default:
+          break;
+      }
+      updateAdmissionDoctors();
+    };
+
+    // Refetch admission data after successful PUT request
+    try {
+      toast.promise(fetchDoctorsData(), toastDetails);
+    } catch (err) {
+      console.error("Error fetching admissions:", err);
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    const search = e.target.value.toLowerCase();
+    const newRows = filterDoctorData(doctorData, search);
+    setFilteredData(newRows);
+  };
 
   const handleClick = () => {
     onOpen();
@@ -37,7 +301,7 @@ export default function DoctorRecord({
       <DataTable
         theme={theme}
         columns={doctorColumns}
-        data={doctorData}
+        data={filteredData}
         progressPending={isPending}
         fixedHeader
         pagination
@@ -46,10 +310,7 @@ export default function DoctorRecord({
         expandableRows
         expandOnRowClicked
         expandableRowsComponent={({ data }) => (
-          <DoctorRowDetails
-            data={data}
-            handleDoctorUpdate={handleDoctorUpdate}
-          />
+          <DoctorRowDetails data={data} handleDoctorUpdate={handleDataUpdate} />
         )}
         subHeader
         subHeaderAlign="left"
@@ -67,11 +328,18 @@ export default function DoctorRecord({
                   icon={<IconUserPlus />}
                   mb="12px"
                 />
-                <Button>On Duty</Button>
-                <Button>Off Duty</Button>
-                <Button colorScheme="green">Active</Button>
-                <Button colorScheme="red">Inactive</Button>
-                <Button colorScheme="yellow">On Leave</Button>
+                <Button onClick={handleGetDoctors}>All Doctors</Button>
+                <Button onClick={handleGetOnDutyDoctors}>On Duty</Button>
+                <Button onClick={handleGetOffDutyDoctors}>Off Duty</Button>
+                <Button onClick={handleGetActiveDoctors} colorScheme="green">
+                  Active
+                </Button>
+                <Button onClick={handleGetInactiveDoctors} colorScheme="red">
+                  Inactive
+                </Button>
+                <Button onClick={handleGetOnLeaveDoctors} colorScheme="yellow">
+                  On Leave
+                </Button>
               </Flex>
               <Text size="md" mb="4px">
                 Total Doctors: <b>{total}</b>
@@ -95,7 +363,7 @@ export default function DoctorRecord({
       <AddDrawer
         isOpen={isOpen}
         onClose={onClose}
-        handleDoctorUpdate={handleDoctorUpdate}
+        handleDoctorUpdate={handleDataUpdate}
       />
     </>
   );
@@ -103,10 +371,5 @@ export default function DoctorRecord({
 
 DoctorRecord.propTypes = {
   theme: PropTypes.string.isRequired,
-  doctorData: PropTypes.array.isRequired,
-  isPending: PropTypes.bool.isRequired,
-  handleDoctorUpdate: PropTypes.func.isRequired,
-  searchTerm: PropTypes.string.isRequired,
-  handleSearch: PropTypes.func.isRequired,
-  total: PropTypes.number.isRequired,
+  updateAdmissionDoctors: PropTypes.func.isRequired,
 };
